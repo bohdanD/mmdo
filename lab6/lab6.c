aa965e43d7eecdd3b5ad7dc60a5fe669fb2da652
bohdanD <doroshbohdan1@gmail.com>
2017-03-29 14:50:00 +0300

    lab6

---
+++ lab6/lab6.c
@@ -0,0 +1,167 @@
+/*
+ * lab6.c
+ * 
+ * Copyright 2017 bohdan <bohdan@bohdan-Lenovo-V580c>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define m 3
+#define n 5
+
+void scanData(double **C, double *A, double *B){
+	FILE *fc = fopen("C.txt", "r");
+	FILE *fa = fopen("A.txt", "r");
+	FILE *fb = fopen("B.txt", "r");
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			fscanf(fc, "%lf\t", &C[i][j]);
+		}
+		fscanf(fc, "\n");
+		fscanf(fa, "%lf\t", &A[i]);
+	}
+	for(int i = 0; i < n; i++)
+		fscanf(fb, "%lf\t", &B[i]);
+	fclose(fc);
+	fclose(fa);
+	fclose(fb);
+}
+
+void printTable(double **C, double *A, double *B){
+	for(int i=0; i<m; i++){
+		for(int j=0; j<n; j++){
+			printf("%.2lf\t", C[i][j]);
+		}
+		printf("\n");	
+	}
+	printf("A: ");
+	for(int i = 0; i < m; i++)
+		printf("%.2lf\t", A[i]);
+	printf("\nB : ");
+	for(int i = 0; i < n; i++)
+		printf("%.2lf\t", B[i]);
+	printf("\n");
+}
+
+void checkOpen(double *A, double *B){
+	double sumA = 0, sumB = 0;
+	for(int i = 0; i < m; i++)
+		sumA += A[i];
+	for(int i = 0; i < n; i++)
+		sumB += B[i];
+	if(sumA == sumB) printf("Closed!");
+	else printf("Open!");
+}
+
+void methodNorhWest(double **X, double *A, double *B){
+	for(int i=0; i<m; i++){
+		for(int j=0; j<n; j++){
+			X[i][j] = -1;
+		}
+	}
+	int i =0;
+	int j=0;
+	while(i != m && j != n){
+		if(A[i] < B[j]){
+			X[i][j] = A[i];
+			B[j] -= X[i][j];
+			i++;
+		}else if(A[i] > B[j]){
+			X[i][j] = B[j];
+			A[i] -= X[i][j];
+			j++;
+		}else if(A[i] == B[j]){
+			X[i][j] =A[i];
+			A[i] = 0;
+			j++;
+		}
+	}
+}
+
+void printX(double **X){
+	printf("\n");
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++)
+			printf("%.2lf\t", X[i][j]);
+		printf("\n");
+	}
+}
+
+void findUV(double **C, double **X, double *U, double *V){
+		for(int i=0; i<m; i++){
+			U[i] = -1;
+		}
+		for(int i=0; i<n; i++){
+			V[i] =-1;
+		}
+		U[0] = 0;
+		V[0] = C[0][0];
+		for(int j=0; j<n; j++){
+			if(C[0][j]>=0){
+				V[j] = C[0][j] - U[0];
+			}
+		}
+		for(int i=1; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(X[i][j]>=0){
+					if(V[j] != -1){
+						U[i] = C[i][j] - V[j];
+					}
+				}
+			}
+		}
+}
+
+int main(int argc, char **argv)
+{
+	double **C;
+	C = (double**)malloc(m * sizeof(double*));
+	for(int i = 0; i < m; i++)
+		C[i] = (double*)malloc(n * sizeof(double));
+	double **X;
+	X = (double**)malloc(m * sizeof(double*));
+	for(int i = 0; i < m; i++)
+		X[i] = (double*)malloc(n * sizeof(double));
+	double **Z;
+	Z = (double**)malloc(m * sizeof(double*));
+	for(int i = 0; i < m; i++)
+		Z[i] = (double*)malloc(n * sizeof(double));
+	for(int i = 0; i < m; i++)
+		for(int j = 0; j < n; j++)
+			Z[i][j] = -1;
+	double **delta;
+	delta = (double**)malloc(m * sizeof(double*));
+	for(int i = 0; i < m; i++)
+		delta[i] = (double*)malloc(n * sizeof(double));
+	double* A = malloc(m * sizeof(double));
+	double* B = malloc(n * sizeof(double));
+	scanData(C, A, B);
+	printTable(C, A, B);
+	checkOpen(A, B);
+	methodNorhWest(X, A, B);
+	printX(X);
+	double* U = malloc(m * sizeof(double));
+	double* V = malloc(n * sizeof(double));
+	
+	return 0;
+}
+
